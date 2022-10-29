75ec05c7ac82bd6ef386a7368f141be5812d3c4e
Bertin Nicolas <nicolas.bertin@univ-rouen.fr>
2022-10-29 09:38:19 +0200

    init: Project Initialization

---
+++ src/test/java/com/fullstack/fullstackproject/FullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.fullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
